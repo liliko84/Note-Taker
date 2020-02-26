fc30122315f7fc9914c9a6fd08bcd0cd94075b19
lili gugushvili <lilikog@yahoo.com>
2020-02-25 19:59:13 -0500

    everything is working

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Note-Taker
+
+
+This is an application that is used to write, save, and delete notes. This application will use an express backend and save and retrieve note data from a JSON file.We set the port, all the routs amd middleware.Seperated the code in different files and connected them to each other.
+
+I created  two folders for routes, such as:apiroutes and html routs.Also the json file for notes.When we get notes it returns notes.html file.
+
+For posting the new note I created the read file in the post that was using my notes and push everything wright before the write file.
+
+As a bonus I also  deleting the notes 
+
+AS a user, you are  able to write,save and delete notes,
+so that you can organize your thoughts and keep track of tasks you need to complete.
+
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,66 @@
 const router = require('express').Router()
-
+const fs = require('fs')
 const path = require('path')
+let notes = require("../DB/notes.json");
 
-router.get('/notes', function(req, res){
-  res.sendFile(path.join(__dirname, '../public/note.html'))
+router.get('/notes', function (req, res) {
+  res.json(notes)
+})
+
+router.delete('/notes/:id', function (req, res) {
+  notes = notes.filter(note => note.id !== parseInt(req.params.id))
+
+  fs.writeFile(path.join(__dirname, "../DB/notes.json"), JSON.stringify(notes),
+    err => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+      res.status(200).json({
+        message: "Deleted Recipe!"
+      })
+    })
+})
+
+// ['dog', 'cat']  length: 2
+// arr[0] === 'dog'
+// arr[1] === 'cat'
+// arr[2] === 'iguana'
+// ['dog', 'cat', 'iguana']
+
+// Problems with .length
+// # Only works when initial ID is zero AND no elements removed
+
+// Benefits of .length
+// # If previous rules follewed, all new id's will be one greater than last element of the arrays id
+
+// Develop algorithm that takes advantage of benefits of .length, without negatives
+// # Get last element of array, make new id one bigger than that id
+
+// ['dog', 'cat'] length: 2
+// arr[1] === 'cat'
+// arr[arr.length - 1] === 'cat'
+// ['dog', 'cat', 'iguana'] length: 3
+// arr[2] === 'iguana'
+// arr[arr.length - 1] === 'iguana'
+
+
+router.post("/notes", function (req, res) {
+  const newNote = {
+    ...req.body,
+    id: notes[notes.length - 1].id + 1
+  };
+
+  notes.push(newNote);
+
+  fs.writeFile(path.join(__dirname, "../DB/notes.json"), JSON.stringify(notes),
+    err => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+      res.status(200).json({
+        message: "New Recipe!"
+      })
+    })
 })
 
 module.exports = router
